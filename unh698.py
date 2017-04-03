6a7e7fd573b49577e6d6b18869c2450f13040264
lozeki <minhtri.ng@gmail.com>
2017-04-03 19:22:51 -0400

    change the return of unh698.py

--- unh698.py
+++ unh698.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hey there!"
+    return "UNH698 Website"
 if __name__ == '__main__':
     app.run(debug=True)
