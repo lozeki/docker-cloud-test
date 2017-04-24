6738d558c2d232abeb155177cbefe16ca73d21e7
lozeki <minhtri.ng@gmail.com>
2017-04-24 14:10:57 -0400

    edit uhn698.py -fix the function name

--- unh698.py
+++ unh698.py
@@ -2,17 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def home():
-    return "UNH698 Website"
 
 @app.route('/')
 def marvel():
     return render_template('marvel.html')
 
 @app.route('/character')
-def marvel():
+def character():
     return render_template('character.html')
 
 if __name__ == '__main__':
