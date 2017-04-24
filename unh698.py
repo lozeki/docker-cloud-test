8537ed43479381580a2876d05d1ca53ed6bd9a2f
lozeki <minhtri.ng@gmail.com>
2017-04-24 13:05:32 -0400

    Add htmls file to the templates and change the unh698.py and unh698_test.py

--- unh698.py
+++ unh698.py
@@ -1,7 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
+
 def home():
     return "UNH698 Website"
+
+@app.route('/marvel')
+def marvel():
+    return render_template('marvel.html')
+
+@app.route('/character')
+def marvel():
+    return render_template('character.html')
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
