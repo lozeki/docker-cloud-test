46d328e14c6e7764538eaca55808f42c9227e89e
lozeki <minhtri.ng@gmail.com>
2017-04-26 21:41:51 -0400

    Fix the links problem

--- unh698.py
+++ unh698.py
@@ -10,9 +10,9 @@ def home():
 @app.route('/movie')
 def movie():
     filmlist = (
-                 {'name':'Doctor Strange','release':'Nov 4, 2016','image':'doctor.jpg'},
-                 {'name':'Captain America: Civil War','release':'May 6, 2016','image':'captain.jpg'},
-                 {'name':'Ant-Man','release':'Jul 17, 2015','image':'ant.jpg'},                 
+                 {'name':'Doctor Strange','release':'Nov 4, 2016','image':'https://i.annihil.us/u/prod/marvel/i/mg/3/50/57fd2d2084523/portrait_incredible.jpg'},
+                 {'name':'Captain America: Civil War','release':'May 6, 2016','image':'https://i.annihil.us/u/prod/marvel/i/mg/c/30/521f6f84ecba1/portrait_incredible.jpg'},
+                 {'name':'Ant-Man','release':'Jul 17, 2015','image':'https://i.annihil.us/u/prod/marvel/i/mg/d/60/5553932315f24/portrait_incredible.jpg'},                 
                )
     return render_template('movie.html',list=filmlist)
 
